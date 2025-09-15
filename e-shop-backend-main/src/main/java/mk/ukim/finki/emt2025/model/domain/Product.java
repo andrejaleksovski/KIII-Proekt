db828f22926e21df1483c130457fc9b379717433
Alekso93 <aleksovskiandrej93@gmail.com>
2025-09-15 21:52:10 +0200

    Initial Commit

---
+++ e-shop-backend-main/src/main/java/mk/ukim/finki/emt2025/model/domain/Product.java
@@ -0,0 +1,107 @@
+package mk.ukim.finki.emt2025.model.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private Integer quantity;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Manufacturer manufacturer;
+
+    public Product() {
+    }
+
+    public Product(
+            String name,
+            Double price,
+            Integer quantity,
+            Category category,
+            Manufacturer manufacturer
+    ) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.manufacturer = manufacturer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Product(
+            Long id,
+            String name,
+            Double price,
+            Integer quantity,
+            Category category,
+            Manufacturer manufacturer
+    ) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.manufacturer = manufacturer;
+    }
+}
+
